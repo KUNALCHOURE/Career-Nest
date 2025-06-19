de88c36510b250cca1164a7ea1b9ad0e1c6f9d59
KUNAL CHOURE <chourekunal4@gmail.com>
2025-06-19 18:00:07 +0530

    backend url updated

--- Frontend/src/service/api.js
+++ Frontend/src/service/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api=axios.create({
    // baseURL:'http://localhost:3030/api/v1',
-    baseURL:'https://job-board-1-fch2.onrender.com',
+    baseURL:'https://job-board-1-fch2.onrender.com/api/v1',
     withCredentials:true,
 })
 
